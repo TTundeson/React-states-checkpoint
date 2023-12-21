4945721b1d6eb779111f0f522e34da82355ac5f3
TTundeson <141639027+TTundeson@users.noreply.github.com>
2023-12-21 15:52:19 +0100

    first commit

---
+++ react-profile-app/src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return <div className='App'>{/* Your component content goes here */}</div>;
+  }
+}
+
+export default App;
